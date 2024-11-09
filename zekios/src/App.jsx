eb61223d1441378806b24a0d1c37d5e91bdf7545
zak2721 <zekiostheone@gmail.com>
2024-11-09 10:52:19 +0300

    first commit

---
+++ zekios/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='app'>
+      <h1>zekios  </h1>
+
+    </div>
+
+  );
+}
+
+export default App
